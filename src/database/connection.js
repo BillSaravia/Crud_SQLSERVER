220ea53654725929d3bc6d8329c5566637baec5f
Bill Saravia <bill.saravia@tecsup.edu.pe>
2024-04-26 22:53:25 -0500

    cambio

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,14 @@
 import sql from 'mssql';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const dbSettings = {
-    user: "soporte",
-    password: "12345678",
-    server:"BILLSARAVIA",
-    database: "preuba",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
         encrypt: true, // for azure
         trustServerCertificate: true, // change to true for local dev / self-signed certs
